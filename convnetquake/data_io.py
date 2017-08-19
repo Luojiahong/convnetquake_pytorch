d327ddefe4b226500fe5b8932fdfee60fa3a9903
Lijun Zhu <gatechzhu@gmail.com>
2017-08-19 16:06:51 -0400

    Add auxiliary function for data I/O.

---
+++ convnetquake/data_io.py
@@ -0,0 +1,25 @@
+"""
+Auxiliary functions for data I/O.
+"""
+from obspy import read
+import pandas as pd
+
+
+def load_stream(file):
+    st = read(file)
+
+    return st.merge()
+
+
+def load_catalog(file):
+    cat = pd.read_csv(file)
+
+    if 'utc_timestamp' not in cat.columns:
+        raise ValueError("No UTC_timestamp. Check if catalog has been "
+                         "converted.")
+
+    return cat
+
+
+if __name__ == '__main__':
+    pass
