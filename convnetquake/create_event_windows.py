e023919f16b255644082edde0e1eed37c25798a4
Lijun Zhu <gatechzhu@gmail.com>
2017-08-20 14:03:29 -0400

    Create h5 files for each event window.

---
+++ convnetquake/create_event_windows.py
@@ -0,0 +1,51 @@
+"""
+Script to cut event windows from continuous mseed files according to OK
+catalog and store each window in separated file in hdf5 format.
+"""
+import os
+import numpy as np
+from obspy import UTCDateTime
+import deepdish as dd
+from convnetquake.data_io import load_stream, load_catalog
+from convnetquake.aux_catalog import filter_catalog_time
+from convnetquake.aux_stream import preprocess_stream
+
+if __name__ == '__main__':
+    # Open catalog
+    cat = load_catalog('tmp/catalog/OK_clustered.csv')
+
+    # Load stream
+    for root, dirs, files in os.walk('data/streams'):
+        for file in files[:]:
+            sta, time = file.split('.')[0].split('_')
+            # mn, yr = time.split('-')
+
+            # Load stream
+            st = load_stream('/'.join([root, file]))
+
+            # Preprocess stream
+            st = preprocess_stream(st)
+
+            # search catalog
+            filtered_cat = filter_catalog_time(cat,
+                                               [st[0].stats.starttime,
+                                                st[0].stats.endtime])
+
+            for event_id, event_time in enumerate(
+                    filtered_cat['utc_timestamp']):
+                dst_file = "_".join(['tmp/train/events/event', str(sta),
+                                     str(UTCDateTime(event_time))]) + '.h5'
+
+                # if os.path.exists(dst_file):
+                #     continue
+
+                start_time = UTCDateTime(event_time)
+                end_time = start_time + 10
+                st_event = st.slice(start_time, end_time)
+                x_event = st_event[0].data[:1000]
+                y_event = st_event[1].data[:1000]
+                z_event = st_event[2].data[:1000]
+                event_data = np.vstack((x_event, y_event, z_event))
+                cluster_id = filtered_cat['cluster_id'].as_matrix()[event_id]
+
+                dd.io.save(dst_file, (cluster_id, event_data))
